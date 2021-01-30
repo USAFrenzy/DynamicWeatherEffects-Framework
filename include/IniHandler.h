d3f8588acc0dca15bfaf43bab989473f6cfef140
Ryan McCullough <mccullough.ryan123@gmail.com>
2021-01-30 16:42:05 -0500

    Added json lib, updated cmakelists.txt, and just saving progress for the night

--- include/IniHandler.h
+++ include/IniHandler.h
@@ -1,5 +1,28 @@
 #pragma once
+#include "nlohmann/json.hpp"
 
 namespace inihandler {
 
-}
+	namespace config {
+		using json = nlohmann::json;
+
+		enum class Sliders { IncreasedThirst = 0, IncreasedThirstAction, IncreasedThirstNeeds };
+
+		enum class Toggles {
+			isPuddles,
+			isPuddleRunoff,
+			isPuddleFreeze,
+			IsPuddleSublimation,
+			isAridOptionEnabled,
+			isDryWells,
+			isFlammableWood,
+			isThirstIncreased,
+			isDebugEnabled,
+			isModEnabled
+		};
+		// Functions To Map String Values To/From The Toggle State
+		std::string TogOptionToStr(Toggles toggleOption);
+		Toggles TogStrToOption(std::string toggleStr);
+	} // namespace config
+
+} // namespace inihandler
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 ﻿extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
 {
 #ifndef NDEBUG
-	auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
+	auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>( );
 #else
-	auto path = logger::log_directory();
-	if (!path) {
+	auto path = logger::log_directory( );
+	if(!path) {
 		return false;
 	}
 
 	*path /= "DynamicWeatherEffects.log"sv;
-	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string( ), true);
 #endif
 
 	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
@@ -27,17 +27,19 @@
 	logger::info("DynamicWeatherEffects v0.1.0");
 
 	a_info->infoVersion = SKSE::PluginInfo::kVersion;
-	a_info->name = "DynamicWeatherEffects";
-	a_info->version = 1;
+	a_info->name        = "DynamicWeatherEffects";
+	a_info->version     = 1;
 
-	if (a_skse->IsEditor()) {
+	if(a_skse->IsEditor( )) {
 		logger::critical("DynamicWeatherEffects Loaded In Editor - Marking As Incompatible"sv);
 		return false;
 	}
 
-	const auto ver = a_skse->RuntimeVersion();
-	if (ver < SKSE::RUNTIME_1_5_97) {
-		logger::critical(FMT_STRING("Unsupported Runtime Version Detected - Please Update Your SKSE64 Install [Version Detected: {} ]"), ver.string());
+	const auto ver = a_skse->RuntimeVersion( );
+	if(ver < SKSE::RUNTIME_1_5_97) {
+		logger::critical(FMT_STRING("Unsupported Runtime Version Detected - Please Update Your SKSE64 "
+					    "Install [Version Detected: {} ]"),
+				 ver.string( ));
 		return false;
 	}
 
